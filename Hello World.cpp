610e67f6e1ad8aaa4e283f14ff6af2a661556cfc
Tackler Gamer <49683529+TacklerGamers@users.noreply.github.com>
2020-01-12 17:15:17 +0500

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string Hello = "/*You can type anything here just remove this line except ""*/"
+
+    cout << Hello << endl;
+
+    return 0; 
+}
